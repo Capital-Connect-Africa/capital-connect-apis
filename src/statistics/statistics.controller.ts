1e9d26f6130dda1aeee0144dcb91623549ba834e
Victor Karanja <vickyjr88@gmail.com>
2024-08-20 22:25:44 +0300

    Minor fixes to submissions updates and users statistics.

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,6 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { StatisticsService } from './statistics.service';
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { Roles } from "../auth/roles.decorator";
+import { Role } from "../auth/role.enum";
 
+@UseGuards(JwtAuthGuard)
+@Roles(Role.Admin)
 @Controller('statistics')
 export class StatisticsController {
   constructor(private readonly userService: StatisticsService) {}
