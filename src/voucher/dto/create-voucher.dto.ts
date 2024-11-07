5f848ff3dae140106863961c306afdd439066c76
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 15:47:47 +0300

    fix: fixed data attr typo

--- src/voucher/dto/create-voucher.dto.ts
+++ src/voucher/dto/create-voucher.dto.ts
@@ -2,6 +2,7 @@ import { VoucherType } from "../../utils/enums/voucher.type.enum";
 import { IsInt, IsNotEmpty, IsNumber, Max, Min } from "class-validator";
 
 export class CreateVoucherDto{
+
     @IsNotEmpty({message: 'voucher type required*'})
     type: VoucherType;
 
@@ -9,12 +10,22 @@ export class CreateVoucherDto{
     @IsInt({message: 'voucher usage limit must be an integer'})
     maxUses: number;
 
-    @IsNotEmpty({message: "add the voucher's validation rules"})
+    @IsNotEmpty({message: "voucher use limit required*"})
+    @IsNumber({maxDecimalPlaces: 2, }, {message: 'voucher usage limit must be an integer'})
+    maxAmount: number;
+
+    @IsNotEmpty({message: "expiry date is required*"})
+    expiresAt: Date;
+
+    @IsNotEmpty({message: "add the voucher's validation rules*"})
     rules: number[]
     
+
+
     @IsNotEmpty({message: 'voucher discount is required*'})
     @IsNumber({maxDecimalPlaces: 2, }, {message: 'discount must be a number'})
     @Min(0, {message: 'dicount percentage cannot be less than 0'})
     @Max(100, {message: 'dicount percentage cannot exceed 100'})
-    percentDiscount: number; // as percentage of the package cost (0-100)%
+    percentageDiscount: number; // as percentage of the package cost (0-100)%
+
 }
