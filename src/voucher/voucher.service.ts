a128801383102f9aa2eb182ec120877a00555664
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 12:08:16 +0300

    chore: added a method to get all vouchers

--- src/voucher/voucher.service.ts
+++ src/voucher/voucher.service.ts
@@ -1,4 +1,25 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Voucher } from './entities/voucher.entity';
 
 @Injectable()
-export class VoucherService {}
+export class VoucherService {
+
+    constructor(
+        @InjectRepository(Voucher)
+        private voucherRepository: Repository<Voucher>,
+    ) {}
+
+    async findAll(): Promise<Voucher[]>{
+        const vouchers =await this.voucherRepository.find({
+            relations: ['rules'],
+            order: {
+                id: 'DESC'
+            },
+            
+        })
+
+        return vouchers
+    }
+}
