97758c8094ca3e803db8fa1bf29708a7be990fde
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 15:47:11 +0300

    chore: created the eligibility rules dto

---
+++ src/voucher/dto/eligibility-rules.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty} from "class-validator";
+import { Operators } from "src/utils/enums/operators.enum";
+
+export class CreateEligibilityRuleDto{
+
+    @IsNotEmpty({message: 'user property required*'})
+    userProperty: string;
+
+    @IsNotEmpty({message: 'operator is required*'})
+    operator: Operators;
+
+    @IsNotEmpty({message: 'value required*'})
+    value: any;
+
+}
