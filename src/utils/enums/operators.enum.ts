073709e2c664ae040698b7b1c7f3a4837b7194a8
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 12:07:23 +0300

    chore: created an operators enum

---
+++ src/utils/enums/operators.enum.ts
@@ -0,0 +1,9 @@
+export enum Operators{
+    EQUAL_TO ='eq',
+    LESS_THAN ='lt',
+    GREATER_THAN ='gt',
+    LESS_THAN_OR_EQUAL_TO ='lte',
+    GREATER_THAN_OR_EQUAL_TO ='gte',
+    BETWEEN ='between',
+    EXISTS ='exists,'
+}
