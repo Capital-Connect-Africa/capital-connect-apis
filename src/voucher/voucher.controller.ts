34bccea324f0ea1d9bb9370b80fb3c67f12a6ac1
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 18:00:33 +0300

    chore: added capability to save code

--- src/voucher/voucher.controller.ts
+++ src/voucher/voucher.controller.ts
@@ -2,6 +2,7 @@ import { VoucherService } from './voucher.service';
 import { CreateVoucherDto } from './dto/create-voucher.dto';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateEligibilityRuleDto } from './dto/eligibility-rules.dto';
+import { generateCryptCode } from 'src/utils/helpers/crypto-generator.helper';
 
 @Controller('vouchers')
 export class VoucherController {
@@ -10,10 +11,13 @@ export class VoucherController {
 
     @Post()
     async createVoucher(@Body() body: CreateVoucherDto){
-        const { type, percentageDiscount, maxUses } =body;
-
+        const { type, percentageDiscount, maxUses, expiresAt, rules, maxAmount } =body;
+        const code =generateCryptCode().toUpperCase();
         
-        return body
+        return await this.service.createVoucher(
+            { type, percentageDiscount, maxUses, expiresAt, code, maxAmount  }, 
+            rules
+        )
     }
 
     @Post('/eligibility-rule')
