f0ab5d3f45fca56cb14590acfd653b09f62d0a5c
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 15:48:33 +0300

    chore: added functionality to create eligibility rule

--- src/voucher/voucher.controller.ts
+++ src/voucher/voucher.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateVoucherDto } from './dto/create-voucher.dto';
 import { VoucherService } from './voucher.service';
+import { CreateVoucherDto } from './dto/create-voucher.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateEligibilityRuleDto } from './dto/eligibility-rules.dto';
 
 @Controller('vouchers')
 export class VoucherController {
@@ -8,13 +9,22 @@ export class VoucherController {
     constructor(private service: VoucherService){}
 
     @Post()
-    create(@Body() body: CreateVoucherDto){
-        return 'Hello, world'
+    async createVoucher(@Body() body: CreateVoucherDto){
+        const { type, percentageDiscount, maxUses } =body;
+
+        
+        return body
     }
 
+    @Post('/eligibility-rule')
+    async createEligilityRule(@Body() body: CreateEligibilityRuleDto){
+        return await this.service.createEligibilityRule(body)
+    }
+
+
     @Get()
-    async findall(){
-        return await this.service.findAll();
+    async findallVouchers(){
+        return await this.service.findAllVouchers();
     }
 
     @Post()
