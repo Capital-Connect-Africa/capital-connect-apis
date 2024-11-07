9f195eabb168620f9814b0131dbfcaaef1690cac
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 15:49:04 +0300

    chore: registered eligiblity rule entity

--- src/voucher/voucher.module.ts
+++ src/voucher/voucher.module.ts
@@ -3,11 +3,12 @@ import { VoucherController } from './voucher.controller';
 import { VoucherService } from './voucher.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Voucher } from './entities/voucher.entity';
+import { EligibilityRule } from './entities/eligibility-rule.entity';
 
 @Module({
   controllers: [VoucherController],
   providers: [VoucherService, ],
-  imports: [TypeOrmModule.forFeature([Voucher])],
+  imports: [TypeOrmModule.forFeature([Voucher, EligibilityRule])],
   exports: [VoucherService]
 })
 export class VoucherModule {}
