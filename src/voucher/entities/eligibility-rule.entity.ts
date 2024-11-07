3ceb2c6864bfcb475eef0af09647fe94caafa649
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 12:12:17 +0300

    chore: created a voucher rules entity

--- src/voucher/entities/eligibility-rule.entity.ts
+++ src/voucher/entities/eligibility-rule.entity.ts
@@ -1,25 +1,31 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Voucher } from "./voucher.entity";
+import { Operators } from "src/utils/enums/operators.enum";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
 
 @Entity('rules')
-export class EligibilityRule{
+export class EligibilityRule {
     @PrimaryGeneratedColumn()
     id: number;
-
+    
     @Column()
     userProperty: string;
 
-    @Column()
-    operator: string; // >, <, =, contains etc
+    @Column({
+        type: 'enum',
+        enum: Operators
+    })
+    operator: Operators;
 
     @Column()
-    value: string;
-
-    @ManyToMany(() =>Voucher, voucher => voucher.rules)
-    vouchers: Voucher[]
+    value: string; // can be range i.e val1 - val2
+    @ManyToMany(() => Voucher, voucher => voucher.rules)
+    vouchers: Voucher[];
 }
 
 // 1. new users -> createdAt
 // 2. users refered by -> referralId
 // 3. roles
 // 4. package
+
+// 5 .150
