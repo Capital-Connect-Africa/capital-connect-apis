6b3d790f86cbcd3e8d634818c2e096490919d82a
meltus-bwire <meltusbwire10@gmail.com>
2024-11-07 12:25:05 +0300

    chore: added capability to fetch all vouchers

--- src/voucher/voucher.controller.ts
+++ src/voucher/voucher.controller.ts
@@ -1,16 +1,20 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateVoucherDto } from './dto/create-voucher.dto';
+import { VoucherService } from './voucher.service';
 
 @Controller('vouchers')
 export class VoucherController {
+
+    constructor(private service: VoucherService){}
+
     @Post()
-    createVoucher(@Body() body: CreateVoucherDto){
+    create(@Body() body: CreateVoucherDto){
         return 'Hello, world'
     }
 
     @Get()
-    getVoucher(){
-        return ''
+    async findall(){
+        return await this.service.findAll();
     }
 
     @Post()
