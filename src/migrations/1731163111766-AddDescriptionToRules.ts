9534e20675f41f4602474d058f9a5938489fe833
meltus-bwire <meltusbwire10@gmail.com>
2024-11-09 19:05:30 +0300

    chore: added a migration for the rule description

---
+++ src/migrations/1731163111766-AddDescriptionToRules.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddDescriptionToRules1731163111766 implements MigrationInterface {
+    name = 'AddDescriptionToRules1731163111766'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "rules" ADD "description" character varying(255) NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "rules" DROP COLUMN "description"`);
+    }
+
+}
