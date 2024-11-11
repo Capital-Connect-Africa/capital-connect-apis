7a5cc8823db0c00375c5728055dbb6dad6aea2c4
meltus-bwire <meltusbwire10@gmail.com>
2024-11-11 17:41:17 +0300

    chore: intialized the swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { CustomLogger } from './shared/utils/custom-logger.util';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error'], // Disable default logger
   });
+
+  // config the documentation
+  const config =new DocumentBuilder()
+              .setTitle('Capital Connect')
+              .setDescription('Backend API REST Endpoints Documentation')
+              .setVersion('1.0.0')
+              .build()
+  const document =SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document)
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
