fd518f0bfa118bbfdc5c8c64f433ccb15d777ab8
ian irungu <gatumaian@gmail.com>
2024-11-20 11:11:30 +0300

    small change on company findOne method.

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -54,10 +54,7 @@ export class CompanyService {
   }
 
   async findOne(id: number){
-    const company = await this.companyRepository.findOne({
-      where: { id, isHidden: false },
-      relations: ['associatedUser'], 
-    });
+    const company = await this.companyRepository.findOneBy({ id });
   
     if (!company) {
       throw new NotFoundException('Company not found');
