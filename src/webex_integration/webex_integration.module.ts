1c73523f215aeda86a474f0950d2b5c787579508
meltus-bwire <meltusbwire10@gmail.com>
2024-12-03 09:39:07 +0300

    fix: fixed webex integration issue

--- src/webex_integration/webex_integration.module.ts
+++ src/webex_integration/webex_integration.module.ts
@@ -6,11 +6,16 @@ import { HttpModule } from '@nestjs/axios';
 import { BookingService } from '../booking/booking.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Booking } from '../booking/entities/booking.entity';
+import { VoucherService } from 'src/voucher/voucher.service';
+import { Voucher } from 'src/voucher/entities/voucher.entity';
+import { EligibilityRule } from 'src/voucher/entities/eligibility-rule.entity';
+import { User } from 'src/users/entities/user.entity';
+import { UserVoucher } from 'src/voucher/entities/user-voucher.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking]), HttpModule],
+  imports: [TypeOrmModule.forFeature([Booking, Voucher, EligibilityRule, User, UserVoucher]), HttpModule],
   controllers: [WebexIntegrationController],
-  providers: [WebexIntegrationService, BookingService],
+  providers: [WebexIntegrationService, BookingService, VoucherService],
 })
 export class WebexIntegrationModule {
   configure(consumer: MiddlewareConsumer) {
