9a10d428d04236d6f2012f7059cf0317d8e31f2d
ian irungu <gatumaian@gmail.com>
2024-12-04 11:56:30 +0300

    updated migration file to update available funding type

--- src/migrations/1733224452820-UpdateInvestorProfilesTable.ts
+++ src/migrations/1733224452820-UpdateInvestorProfilesTable.ts
@@ -1,15 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
-
 export class UpdateInvestorProfilesTable1733224452820 implements MigrationInterface {
     name = 'UpdateInvestorProfilesTable1733224452820'
-
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "investor_profiles" ADD "availableFundingBigInt" bigint`);
+        await queryRunner.query(`UPDATE "investor_profiles" SET "availableFundingBigInt" = CAST("availableFunding" AS bigint)`);
         await queryRunner.query(`ALTER TABLE "investor_profiles" DROP COLUMN "availableFunding"`);
-        await queryRunner.query(`ALTER TABLE "investor_profiles" ADD "availableFunding" bigint NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "investor_profiles" RENAME COLUMN "availableFundingBigInt" TO "availableFunding"`);
     }
-
+    
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "investor_profiles" ADD "availableFunding" integer`);
+        await queryRunner.query(`UPDATE "investor_profiles" SET "availableFunding" = CAST("availableFunding" AS integer)`);
         await queryRunner.query(`ALTER TABLE "investor_profiles" DROP COLUMN "availableFunding"`);
-        await queryRunner.query(`ALTER TABLE "investor_profiles" ADD "availableFunding" integer NOT NULL`);
     }
 }
