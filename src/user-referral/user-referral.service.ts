5d75554e753c04cfede2e91892c6434d25279a3d
meltus-bwire <meltusbwire10@gmail.com>
2024-12-18 00:44:29 +0300

    chore: added implementations for updating metrics, and fetching referrals

--- src/user-referral/user-referral.service.ts
+++ src/user-referral/user-referral.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Referral } from './entities/referral.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UpdateReferralMetricsDto } from './dto/UpdateReferralMetricsDto';
 
 @Injectable()
-export class UserReferralService {}
+export class UserReferralService {
+
+    constructor(
+        @InjectRepository(Referral)
+        private readonly userReferralRepository: Repository<Referral>
+    ){}
+
+    async findReferrals(page:number =1, limit:number =10): Promise<Referral[]>{
+        try {
+            const skip = (page -1) *limit
+            const referrals =await this.userReferralRepository.find({
+                skip,
+                take: limit,
+                relations: ['users'],
+                order: {
+                    id: 'DESC'
+                },
+            });
+            return referrals;  
+        } catch (error) {
+            throw error as InternalServerErrorException
+        }
+    }
+
+    async findUserReferrals(userId:number, page:number =1, limit:number =10): Promise<Referral[]>{
+        try {
+            const skip = (page -1) *limit;
+            const referrals =await this.userReferralRepository.find({
+                skip,
+                take: limit,
+                where: {user: {id: userId}},
+                relations: ['user', 'user.r'],
+                order: {
+                    id: 'DESC'
+                },
+            });
+            return referrals;
+            
+        } catch (error) {
+            throw error as InternalServerErrorException
+        }
+    }
+
+    async updateUserReferrals(userId:number, body: UpdateReferralMetricsDto){
+        try {
+            const referral =await this.userReferralRepository.findOneBy({id: userId});
+            if(referral){
+                let userReferralLinkClicks =referral.clicks;
+                let userReferralLinkVisits =referral.visits;
+                const {clicks, visits} =body;
+                if(clicks) userReferralLinkClicks +=1
+                if(visits) userReferralLinkVisits +=1
+
+                await this.userReferralRepository.update(
+                    userId, 
+                    {
+                        clicks: userReferralLinkClicks, 
+                        visits: userReferralLinkVisits
+                    }
+                )
+
+            }
+        } catch (error) {
+            throw error as BadRequestException
+        }
+    }
+}
