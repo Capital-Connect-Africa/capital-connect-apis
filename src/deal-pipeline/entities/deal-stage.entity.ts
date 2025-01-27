978f2a59fb71ab972194fbefcd9541cfe00d9144
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-27 09:44:38 +0300

    chore: created the deal stage entity

---
+++ src/deal-pipeline/entities/deal-stage.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Deal } from './deal.entity';
+import { DealStageHistory } from './deal-stage-history.entity';
+
+@Entity('deal-stages')
+export class DealStage {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ type: 'decimal', precision: 20, scale: 2 }) // for huge value caps
+  value: number;
+
+  @Column({ type: 'int', width: 3 }) // Progress as percentage (0-100)
+  progress: number;
+
+  @OneToMany(() => Deal, (deal) => deal.stage)
+  deals: Deal[];
+
+  @Column({ type: 'timestamp', nullable: true })
+  deadline: Date;
+
+  @OneToMany(() => DealStageHistory, (history) => history.stage)
+  stageHistory: DealStageHistory[];
+}
