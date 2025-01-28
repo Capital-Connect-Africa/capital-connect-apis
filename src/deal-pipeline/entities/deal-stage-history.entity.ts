1b6b9c35b42f011fc4f81957504818c6019f58b5
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-28 23:56:23 +0300

    refactor: changed the stage type

--- src/deal-pipeline/entities/deal-stage-history.entity.ts
+++ src/deal-pipeline/entities/deal-stage-history.entity.ts
@@ -6,7 +6,7 @@ import {
   Column,
 } from 'typeorm';
 import { Deal } from './deal.entity';
-import { Stage } from 'src/stage/entities/stage.entity';
+import { DealStage } from './deal-stage.entity';
 
 @Entity('deal-stage-history')
 export class DealStageHistory {
@@ -16,14 +16,14 @@ export class DealStageHistory {
   @ManyToOne(() => Deal, (deal) => deal.history, { onDelete: 'CASCADE' })
   deal: Deal;
 
-  @ManyToOne(() => Stage, { nullable: true, onDelete: 'SET NULL' })
-  fromStage: Stage;
+  @ManyToOne(() => DealStage, { nullable: true, onDelete: 'SET NULL' })
+  fromStage: DealStage;
 
   @Column({ type: 'decimal', precision: 20, scale: 2, default: 0.0 }) // keeps track of changes in deal value through stages
   valueShift: number;
 
-  @ManyToOne(() => Stage, { nullable: false, onDelete: 'CASCADE' })
-  toStage: Stage;
+  @ManyToOne(() => DealStage, { nullable: false, onDelete: 'CASCADE' })
+  toStage: DealStage;
 
   @CreateDateColumn()
   movedAt: Date;
