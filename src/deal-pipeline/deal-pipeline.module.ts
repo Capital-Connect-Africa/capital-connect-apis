7b0d8e881018c6dead778eba35d69d17d8d722b0
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-28 23:55:29 +0300

    chore: added deps to the deal-pipeline module

--- src/deal-pipeline/deal-pipeline.module.ts
+++ src/deal-pipeline/deal-pipeline.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { DealPipelineService } from './deal-pipeline.service';
 import { DealPipelineController } from './deal-pipeline.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Deal } from './entities/deal.entity';
+import { DealStage } from './entities/deal-stage.entity';
+import { User } from 'src/users/entities/user.entity';
+import { DealAttachment } from './entities/deal-attachments.entity';
+import { DealStageHistory } from './entities/deal-stage-history.entity';
+import { DealCustomer } from './entities/deal-customer.entity';
 
 @Module({
   providers: [DealPipelineService],
-  controllers: [DealPipelineController]
+  controllers: [DealPipelineController],
+  exports: [DealPipelineService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Deal,
+      DealStage,
+      User,
+      DealCustomer,
+      DealAttachment,
+      DealStageHistory,
+    ]),
+  ],
 })
 export class DealPipelineModule {}
