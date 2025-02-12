9fed84e96e6373f1b3ed399b2a8e56eb1f6ac09c
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-12 16:15:32 +0300

    chore: add service and controller to the investors module

--- src/investors-repository/investors-repository.module.ts
+++ src/investors-repository/investors-repository.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { InvestorsRepositoryController } from './investors-repository.controller';
 import { InvestorsRepositoryService } from './investors-repository.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InvestorsRepository } from './entities/investors-repository.entity';
+import { InvestorType } from 'src/investor-types/entities/investor-type.entity';
+import { Sector } from 'src/sector/entities/sector.entity';
+import { SubSector } from 'src/subsector/entities/subsector.entity';
+import { InvestorRespostoryInvestees } from './entities/investor-repository-investees.entity';
 
 @Module({
   controllers: [InvestorsRepositoryController],
-  providers: [InvestorsRepositoryService]
+  providers: [InvestorsRepositoryService],
+  imports: [
+    TypeOrmModule.forFeature([
+      InvestorsRepository,
+      InvestorType,
+      Sector,
+      SubSector,
+      InvestorRespostoryInvestees,
+    ]),
+  ],
+  exports: [InvestorsRepositoryService],
 })
 export class InvestorsRepositoryModule {}
