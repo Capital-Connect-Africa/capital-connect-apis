8340285f888ab4b4e8144d744ec42fc29bcb403a
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-14 08:41:30 +0300

    chore: added routed for the investor global database

--- src/investors-repository/investors-repository.controller.ts
+++ src/investors-repository/investors-repository.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -10,8 +11,11 @@ import {
   Put,
   Query,
   RequestMethod,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
+
 import { InvestorsRepositoryService } from './investors-repository.service';
 import { InvestorsRepository } from './entities/investors-repository.entity';
 import { ErrorDto } from 'src/shared/generic/error.dto';
@@ -33,12 +37,15 @@ import { Roles } from 'src/auth/roles.decorator';
 import { handleError } from 'src/shared/helpers/error-handler.helper';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileParser } from 'src/shared/utils/file-parser.util';
+import { InvestorsUsersSearchHistoryDto } from './dto/investors-users-search-history.dto';
 
-@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('investors repository')
 @Controller('investors-repository')
 export class InvestorsRepositoryController {
   constructor(private investorRespositoryService: InvestorsRepositoryService) {}
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Post()
   @ApiOperation({ summary: 'Adds a new external investor to the repository' })
@@ -96,6 +103,30 @@ export class InvestorsRepositoryController {
     }
   }
 
+  @Post('search')
+  @ApiOperation({ summary: 'Filters investors and create a search entry' })
+  @ApiCreatedResponse({
+    description: 'External investors retrieved successfully',
+    type: InvestorsRepository,
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid data provided',
+    type: ErrorDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'A little server oopsy occured! Not your bad 😃',
+    type: ErrorDto,
+  })
+  async searchExternalInvestors(@Body() body: InvestorsUsersSearchHistoryDto) {
+    try {
+      return await this.investorRespositoryService.searchExternalInvestors(
+        body,
+      );
+    } catch (error) {
+      handleError(error, RequestMethod.POST);
+    }
+  }
+
   @Get(':investorId')
   @ApiOperation({ summary: 'Fetch an external investor by id.' })
   @ApiOkResponse({
@@ -119,6 +150,7 @@ export class InvestorsRepositoryController {
     }
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Put(':investorId')
   @ApiOperation({ summary: 'Update external investor details' })
@@ -156,6 +188,7 @@ export class InvestorsRepositoryController {
     }
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Delete(':investorId')
   @HttpCode(HttpStatus.NO_CONTENT)
@@ -185,6 +218,35 @@ export class InvestorsRepositoryController {
       return await this.investorRespositoryService.removeInvestor(investorId);
     } catch (error) {
       handleError(error, RequestMethod.DELETE);
+    }
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadInvestorsFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) throw new BadRequestException('No file uploaded');
+    const { originalname, buffer } = file;
+    const fileExtension = originalname.split('.').pop().toLowerCase();
+
+    let investorsData: any[];
+
+    try {
+      if (fileExtension === 'json') {
+        investorsData = JSON.parse(buffer.toString());
+      } else if (fileExtension === 'csv') {
+        investorsData = await FileParser.parseCSV(buffer);
+      } else if (fileExtension === 'xlsx') {
+        investorsData = FileParser.parseExcel(buffer);
+      } else {
+        throw new BadRequestException('Unsupported file format');
+      }
+      return await this.investorRespositoryService.bulkCreateInvestors(
+        investorsData,
+      );
+    } catch (error) {
+      throw new BadRequestException('Error processing file: ' + error.message);
     }
   }
 }
