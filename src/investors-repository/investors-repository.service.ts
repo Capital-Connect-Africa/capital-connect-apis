ace8811d3896050ea9f2051e27cc6b9a0bb08b9b
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-14 08:42:22 +0300

    chore: added functionalities to perform crud operations ont the global investor database

--- src/investors-repository/investors-repository.service.ts
+++ src/investors-repository/investors-repository.service.ts
@@ -1,4 +1,10 @@
-import { Repository, In } from 'typeorm';
+import {
+  Repository,
+  In,
+  LessThanOrEqual,
+  MoreThanOrEqual,
+  ArrayContains,
+} from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import {
   BadRequestException,
@@ -11,12 +17,15 @@ import { InvestorsRepository } from './entities/investors-repository.entity';
 import { InvestorType } from 'src/investor-types/entities/investor-type.entity';
 import { Sector } from 'src/sector/entities/sector.entity';
 import { SubSector } from 'src/subsector/entities/subsector.entity';
-import { InvestorRespostoryInvestees } from './entities/investor-repository-investees.entity';
 import { Country } from 'src/country/entities/country.entity';
 import { EsgFocusAreas } from 'src/esg-focus/entities/esg-focus-areas.entity';
 import { Stage } from 'src/stage/entities/stage.entity';
 import { isValidURL } from 'src/shared/helpers/validate-http-url.helper';
 import { textToTitlteCase } from 'src/shared/helpers/text-to-title-case';
+import { InvestorsUsersSearchHistoryDto } from './dto/investors-users-search-history.dto';
+import { InvestmentStructure } from 'src/investment-structures/entities/investment-structure.entity';
+import { UseOfFunds } from 'src/use-of-funds/entities/use-of-funds.entity';
+import { InvestorsRepositorySearchHistory } from './entities/investors-respository-search-history.entity';
 
 @Injectable()
 export class InvestorsRepositoryService {
@@ -29,33 +38,154 @@ export class InvestorsRepositoryService {
     private readonly sectorRepository: Repository<Sector>,
     @InjectRepository(SubSector)
     private readonly subSectorRepository: Repository<SubSector>,
-    @InjectRepository(InvestorRespostoryInvestees)
-    private readonly investorRespostoryInvesteesRepository: Repository<InvestorRespostoryInvestees>,
     @InjectRepository(Country)
     private readonly countryRepository: Repository<Country>,
     @InjectRepository(EsgFocusAreas)
     private readonly esgRepository: Repository<EsgFocusAreas>,
     @InjectRepository(Stage)
     private readonly stageRepository: Repository<Stage>,
+    @InjectRepository(InvestorsRepositorySearchHistory)
+    private readonly userSearchRepository: Repository<InvestorsRepositorySearchHistory>,
+    @InjectRepository(InvestmentStructure)
+    private readonly investmentStructureRespository: Repository<InvestmentStructure>,
+    @InjectRepository(UseOfFunds)
+    private readonly useOfFundsRespository: Repository<UseOfFunds>,
   ) {}
+
+  textToArray(text: string | string[]) {
+    return typeof text === 'string'
+      ? `${text}`.split(',').map((str) => str.trim())
+      : text;
+  }
+
+  private async normalizePayload(payload: Partial<InvestorRepositoryDto>) {
+    const {
+      sectors,
+      subSectors,
+      countries,
+      esgFocusAreas,
+      businessGrowthStages,
+      useOfFunds,
+      investmentStructures,
+      investees,
+      investorType,
+      website,
+      name,
+      contactEmail,
+      contactName,
+      description,
+      fundingVehicle,
+      minFunding,
+      maxFunding,
+      currency,
+    } = payload;
+    const validInfo: Partial<InvestorsRepository> = {};
+
+    if (countries) {
+      validInfo.countries = (
+        await this.countryRepository.find({
+          where: {
+            name: In(this.textToArray(countries)),
+          },
+        })
+      ).map((country) => country.name);
+    }
+
+    if (esgFocusAreas) {
+      validInfo.esgFocusAreas = (
+        await this.esgRepository.find({
+          where: {
+            title: In(this.textToArray(esgFocusAreas)),
+          },
+        })
+      ).map((esg) => esg.title);
+    }
+
+    if (businessGrowthStages) {
+      validInfo.businessGrowthStages = (
+        await this.stageRepository.find({
+          where: {
+            title: In(this.textToArray(businessGrowthStages)),
+          },
+        })
+      ).map((stage) => stage.title);
+    }
+
+    if (sectors) {
+      validInfo.sectors = (
+        await this.sectorRepository.find({
+          where: {
+            name: In(this.textToArray(sectors)),
+          },
+        })
+      ).map((sector) => sector.name);
+    }
+
+    if (subSectors) {
+      validInfo.subSectors = (
+        await this.subSectorRepository.find({
+          where: {
+            name: In(this.textToArray(subSectors)),
+          },
+        })
+      ).map((subSector) => subSector.name);
+    }
+
+    if (useOfFunds) {
+      validInfo.useOfFunds = (
+        await this.useOfFundsRespository.find({
+          where: {
+            title: In(this.textToArray(useOfFunds)),
+          },
+        })
+      ).map((useOfFund) => useOfFund.title);
+    }
+
+    if (investmentStructures) {
+      validInfo.investmentStructures = (
+        await this.investmentStructureRespository.find({
+          where: {
+            title: In(this.textToArray(investmentStructures)),
+          },
+        })
+      ).map((structure) => structure.title);
+    }
+
+    if (investees) {
+      validInfo.investees = this.textToArray(investees).map((investee) =>
+        textToTitlteCase(investee),
+      );
+    }
+
+    if (investorType) {
+      const type = await this.investorTypeRepository.findOneBy({
+        title: investorType.trim(),
+      });
+      if (!type) throw new BadRequestException('Unsupported investor type');
+      validInfo.type = type.title;
+    }
+
+    if (website) {
+      if (!isValidURL(website.toLowerCase()))
+        throw new BadRequestException(`invalid url '${website}'`);
+      validInfo.website = website.toLowerCase();
+    }
+
+    if (name) validInfo.name = textToTitlteCase(name);
+    if (contactName) validInfo.contactName = textToTitlteCase(contactName);
+    if (contactEmail) validInfo.contactEmail = `${contactEmail}`.toLowerCase();
+    if (currency) validInfo.currency = currency;
+    if (description) validInfo.description = description;
+    if (fundingVehicle) validInfo.fundingVehicle = fundingVehicle;
+    if (minFunding) validInfo.minFunding = minFunding;
+    if (maxFunding) validInfo.maxFunding = maxFunding;
+    return validInfo;
+  }
+
   async createInvestor(
     payload: InvestorRepositoryDto,
   ): Promise<InvestorsRepository> {
-    const {
-      sectors,
-      subsectors,
-      website,
-      name,
-      typeId,
-      countries,
-      investees,
-      esgFocusAreas,
-      minFunding,
-      maxFunding,
-      businessGrowthStages,
-      description,
-      fundingVehicle,
-    } = payload;
+    const { name, investorType, minFunding, maxFunding } = payload;
 
     if (!name.trim())
       throw new BadRequestException('investor name is required');
@@ -63,119 +193,43 @@ export class InvestorsRepositoryService {
       throw new BadRequestException('investors minimum funding required');
     if (!maxFunding)
       throw new BadRequestException('investors maximum funding required');
-    if (!typeId) throw new BadRequestException('investors type required');
 
+    if (!investorType) throw new BadRequestException('investors type required');
     if (
       await this.investorsRepository.findOneBy({ name: textToTitlteCase(name) })
     )
       throw new ConflictException('Investor with name already exists');
 
-    const newExternalInvestor: Partial<InvestorsRepository> = {
-      name: textToTitlteCase(name),
-      minFunding,
-      maxFunding,
-      description,
-      fundingVehicle,
-    };
-
-    if (minFunding > maxFunding) {
-      newExternalInvestor.minFunding = maxFunding;
-      newExternalInvestor.maxFunding = minFunding;
-    }
-    const type = await this.investorTypeRepository.findOneBy({ id: typeId });
-    if (!type) throw new BadRequestException('Unsupported investor type');
-    newExternalInvestor.type = type;
-
-    if (website) {
-      if (!isValidURL(website.toLowerCase()))
-        throw new BadRequestException(`invalid url '${website}'`);
-      newExternalInvestor.website = website.toLowerCase();
-    }
-
-    if (countries) {
-      newExternalInvestor.countries = (
-        await this.countryRepository.find({
-          where: {
-            name: In(countries),
-          },
-        })
-      ).map((country) => country.name);
-    }
-
-    if (esgFocusAreas) {
-      newExternalInvestor.esgFocusAreas = (
-        await this.esgRepository.find({
-          where: {
-            title: In(esgFocusAreas),
-          },
-        })
-      ).map((esg) => esg.title);
-    }
-
-    if (businessGrowthStages) {
-      newExternalInvestor.businessGrowthStages = (
-        await this.stageRepository.find({
-          where: {
-            title: In(businessGrowthStages),
-          },
-        })
-      ).map((stage) => stage.title);
-    }
-
-    if (sectors) {
-      newExternalInvestor.sectors = await this.sectorRepository.find({
-        where: {
-          id: In(sectors),
-        },
-      });
-    }
-
-    if (subsectors) {
-      newExternalInvestor.subSectors = await this.subSectorRepository.find({
-        where: {
-          id: In(subsectors),
-        },
-      });
-    }
-
-    if (investees) {
-      const existingInvestees =
-        await this.investorRespostoryInvesteesRepository.find({
-          where: {
-            name: In(investees.map((investee) => textToTitlteCase(investee))),
-          },
-        });
-
-      const existingInvesteesNames = existingInvestees.map((inv) => inv.name);
-      const missingInvesteesNames = investees.filter(
-        (inv) => !existingInvesteesNames.includes(textToTitlteCase(inv)),
-      );
-
-      let newInvestees = [];
-
-      if (missingInvesteesNames.length > 0) {
-        newInvestees = await Promise.all(
-          missingInvesteesNames.map(async (inv) => {
-            const newInvestee =
-              this.investorRespostoryInvesteesRepository.create({
-                name: textToTitlteCase(inv),
-              });
-            return await this.investorRespostoryInvesteesRepository.save(
-              newInvestee,
-            );
-          }),
-        );
-      }
-
-      newExternalInvestor.investees = [...existingInvestees, ...newInvestees];
-    }
-
+    const newExternalInvestor = await this.normalizePayload(payload);
     const externalInvestor =
       this.investorsRepository.create(newExternalInvestor);
     return await this.investorsRepository.save(externalInvestor);
   }
 
-  async createManyInvestors() {}
+  async bulkCreateInvestors(investorsData: InvestorRepositoryDto[]) {
+    if (!Array.isArray(investorsData) || investorsData.length === 0) {
+      throw new BadRequestException('Invalid investors data');
+    }
+
+    const savedInvestors = [];
+    const failedInvestors = [];
+    for (const investorData of investorsData) {
+      try {
+        const savedInvestor = await this.createInvestor(investorData);
+        savedInvestors.push(savedInvestor);
+      } catch (error) {
+        failedInvestors.push({ investorData, error: error.message });
+      }
+    }
+
+    return {
+      message: 'Operation completed',
+      savedCount: savedInvestors.length,
+      failedCount: failedInvestors.length,
+      failedInvestors,
+      savedInvestors,
+    };
+  }
 
   async getInvestors(
     page: number = 1,
@@ -185,7 +239,6 @@ export class InvestorsRepositoryService {
     const [data, total_count] = await this.investorsRepository.findAndCount({
       skip,
       take: limit,
-      relations: ['type', 'investees', 'sectors', 'subSectors'],
     });
     return { data, total_count };
   }
@@ -195,7 +248,6 @@ export class InvestorsRepositoryService {
       where: {
         id: investorId,
       },
-      relations: ['type', 'investees', 'sectors', 'subSectors'],
     });
     if (!investor) throw new NotFoundException(`Investor with not found`);
     return investor;
@@ -205,135 +257,20 @@ export class InvestorsRepositoryService {
     investorId: number,
     payload: Partial<InvestorRepositoryDto>,
   ): Promise<InvestorsRepository> {
-    const investor = await this.investorsRepository.findOne({
+    let investor = await this.investorsRepository.findOne({
       where: {
         id: investorId,
       },
     });
     if (!investor) throw new NotFoundException(`Investor with id not found`);
-    const {
-      sectors,
-      subsectors,
-      website,
-      name,
-      typeId,
-      countries,
-      investees,
-      esgFocusAreas,
-      minFunding,
-      maxFunding,
-      currency,
-      businessGrowthStages,
-      description,
-      fundingVehicle,
-    } = payload;
-
-    if (name) investor.name = textToTitlteCase(name);
-    if (currency) investor.currency = currency;
-    if (description) investor.description = description;
-    if (fundingVehicle) investor.fundingVehicle = fundingVehicle;
-    if (typeId) {
-      const type = await this.investorTypeRepository.findOneBy({ id: typeId });
-      if (!type) throw new BadRequestException('Unsupported investor type');
-      investor.type = type;
-    }
+    const { minFunding, maxFunding } = payload;
     const min_funding = minFunding ?? investor.minFunding;
     const max_funding = maxFunding ?? investor.maxFunding;
     if (min_funding && max_funding && min_funding > max_funding) {
       investor.minFunding = max_funding;
       investor.maxFunding = min_funding;
     }
-    const type = await this.investorTypeRepository.findOneBy({ id: typeId });
-    if (!type) throw new BadRequestException('Unsupported investor type');
-    investor.type = type;
-
-    if (website) {
-      if (!isValidURL(website.toLowerCase()))
-        throw new BadRequestException(`invalid url '${website}'`);
-      investor.website = website.toLowerCase();
-    }
-
-    if (countries) {
-      investor.countries = (
-        await this.countryRepository.find({
-          where: {
-            name: In(countries),
-          },
-        })
-      ).map((country) => country.name);
-    }
-
-    if (esgFocusAreas) {
-      investor.esgFocusAreas = (
-        await this.esgRepository.find({
-          where: {
-            title: In(esgFocusAreas),
-          },
-        })
-      ).map((esg) => esg.title);
-    }
-
-    if (businessGrowthStages) {
-      investor.businessGrowthStages = (
-        await this.stageRepository.find({
-          where: {
-            title: In(businessGrowthStages),
-          },
-        })
-      ).map((stage) => stage.title);
-    }
-
-    if (sectors) {
-      investor.sectors = await this.sectorRepository.find({
-        where: {
-          id: In(sectors),
-        },
-      });
-    }
-
-    if (subsectors) {
-      investor.subSectors = await this.subSectorRepository.find({
-        where: {
-          id: In(subsectors),
-        },
-      });
-    }
-
-    if (investees) {
-      const existingInvestees =
-        await this.investorRespostoryInvesteesRepository.find({
-          where: {
-            name: In(investees.map((investee) => textToTitlteCase(investee))),
-          },
-        });
-
-      const existingInvesteesNames = existingInvestees.map((inv) => inv.name);
-      const missingInvesteesNames = investees.filter(
-        (inv) => !existingInvesteesNames.includes(textToTitlteCase(inv)),
-      );
-
-      let newInvestees = [];
-
-      if (missingInvesteesNames.length > 0) {
-        newInvestees = await Promise.all(
-          missingInvesteesNames.map(async (inv) => {
-            const newInvestee =
-              this.investorRespostoryInvesteesRepository.create({
-                name: textToTitlteCase(inv),
-              });
-            return await this.investorRespostoryInvesteesRepository.save(
-              newInvestee,
-            );
-          }),
-        );
-      }
-
-      investor.investees =
-        await this.investorRespostoryInvesteesRepository.findBy({
-          name: In(investees.map((inv) => textToTitlteCase(inv))),
-        });
-    }
-
+    investor = { ...investor, ...(await this.normalizePayload(payload)) };
     await this.investorsRepository.save(investor);
     return investor;
   }
@@ -346,5 +283,47 @@ export class InvestorsRepositoryService {
     });
     if (!investor) throw new NotFoundException(`Investor with not found`);
     await this.investorsRepository.delete(investorId);
+  }
+
+  async searchExternalInvestors(payload: InvestorsUsersSearchHistoryDto) {
+    const { sector, subSector, country, targetAmount, useOfFunds } = payload;
+    const where: any = {};
+    if (country) where.countries = ArrayContains([country]);
+    if (sector) where.sectors = ArrayContains([sector]);
+    if (subSector) where.subSectors = ArrayContains([subSector]);
+    if (targetAmount) {
+      where.minFunding = LessThanOrEqual(targetAmount);
+      where.maxFunding = MoreThanOrEqual(targetAmount);
+    }
+    if (useOfFunds) where.useOfFunds = ArrayContains([useOfFunds]);
+
+    const [investors, matches] = await this.investorsRepository.findAndCount({
+      where,
+    });
+    const newSearchEntry = this.userSearchRepository.create({
+      sector,
+      country,
+      targetAmount,
+      useOfFunds,
+      subSector,
+      matches,
+    });
+    await this.userSearchRepository.save(newSearchEntry);
+    return investors;
+  }
+
+  async getUserSearchHistories(
+    page: number = 1,
+    limit: number = 1000,
+  ): Promise<{
+    data: InvestorsRepositorySearchHistory[];
+    total_count: number;
+  }> {
+    const skip = (page - 1) * limit;
+    const [data, total_count] = await this.userSearchRepository.findAndCount({
+      skip,
+      take: limit,
+    });
+    return { data, total_count };
   }
 }
