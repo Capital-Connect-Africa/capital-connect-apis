0c26f2ab819daeedc0e897fc919ba37f4efa548b
Victor Karanja <vickyjr88@gmail.com>
2025-02-19 11:46:06 +0300

    Add endpoints for countries, industries and peers.

--- src/valutico/valutico.service.ts
+++ src/valutico/valutico.service.ts
@@ -3,20 +3,39 @@ import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class ValuticoService {
-  private readonly baseUrl = 'https://app.demo.valutico.dev/api/marketplaces/default';
+  private readonly baseUrl = 'https://app.demo.valutico.dev/api';
 
   constructor(private readonly httpService: HttpService) {}
 
   async getMarketplace() {
-    const response = await this.httpService.get(this.baseUrl).toPromise();
+    const url = `${this.baseUrl}/marketplaces/default`;
+    const response = await this.httpService.get(url).toPromise();
     return response.data;
   }
 
   async submitValuation(valuationData: any) {
-    const url = `${this.baseUrl}/valuations`;
+    const url = `${this.baseUrl}/marketplaces/default/valuations`;
     const response = await this.httpService
       .post(url, valuationData)
       .toPromise();
+    return response.data;
+  }
+
+  async getCountries() {
+    const url = `${this.baseUrl}/finance_countries?term=:param&flat=true&locale=en`;
+    const response = await this.httpService.get(url).toPromise();
+    return response.data;
+  }
+
+  async getIndustries() {
+    const url = `${this.baseUrl}/nace_industries?term=:param&flat=true&locale=en`;
+    const response = await this.httpService.get(url).toPromise();
+    return response.data;
+  }
+
+  async getPeers() {
+    const url = `${this.baseUrl}/peers?term=:param&flat=true`;
+    const response = await this.httpService.get(url).toPromise();
     return response.data;
   }
 }
