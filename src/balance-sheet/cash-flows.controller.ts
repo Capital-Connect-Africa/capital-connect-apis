db981ecb1fab7c2f3558fd44243f70f732cf2e99
ian irungu <gatumaian@gmail.com>
2025-02-19 07:15:57 +0300

    Added guards.

--- src/balance-sheet/cash-flows.controller.ts
+++ src/balance-sheet/cash-flows.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, UseGuards } from "@nestjs/common";
 import { CashflowService } from "./cash-flows.service";
 import { CashflowStatement } from "./entities/cash-flows.entity";
+import { RolesGuard } from "src/auth/roles.guard";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('cashflow')
 export class CashflowController {
   constructor(private readonly cashflowService: CashflowService) {}
